50ebf49382afd5b4534be7234a78aff16719b2d0
Slobodan Kovrlija <slobkov02@yahoo.com>
2017-09-29 17:56:55 -0400

    Add ui elements to welcome view

--- musearch/musearch/WelcomeViewController.swift
+++ musearch/musearch/WelcomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WelcomeViewController.swift
 //  musearch
 //
 //  Created by Slobodan Kovrlija on 9/29/17.
@@ -7,19 +7,19 @@
 //
 
 import UIKit
+import Foundation
 
-class ViewController: UIViewController {
-
+class WelcomeViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
