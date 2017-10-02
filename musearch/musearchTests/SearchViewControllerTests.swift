2a530dbfd82131a375c4b9e760b1c206762f926a
Slobodan Kovrlija <slobkov02@yahoo.com>
2017-10-01 20:42:50 -0400

    Add url string check test

--- musearch/musearchTests/SearchViewControllerTests.swift
+++ musearch/musearchTests/SearchViewControllerTests.swift
@@ -7,12 +7,16 @@
 //
 
 import XCTest
+@testable import musearch
 
 class SearchViewControllerTests: XCTestCase {
     
+    var searchViewController: SearchViewController!
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        searchViewController = SearchViewController()
     }
     
     override func tearDown() {
@@ -20,8 +24,25 @@ class SearchViewControllerTests: XCTestCase {
         super.tearDown()
     }
     
-    func test_prepareEntered_ReturnsStringWithoutSpacesAndQuotes {
+    func test_prepareEntered_ReturnsStringWithoutSpacesAndSpecChars() {
         
+        let string1 = "born again"
+        let string2 = "like a prayer"
+        let string3 = "Moore’s" // Note differnce btwn '(allowed) and ’(not allowed)
+        let string4 = "Guns’n Roses"
+        let string5 = "Housecat{}?"
+        
+        let converted1 = searchViewController.prepareEntered(string: string1)
+        let converted2 = searchViewController.prepareEntered(string: string2)
+        let converted3 = searchViewController.prepareEntered(string: string3)
+        let converted4 = searchViewController.prepareEntered(string: string4)
+        let converted5 = searchViewController.prepareEntered(string: string5)
+        
+        XCTAssertEqual(converted1, "born+again")
+        XCTAssertEqual(converted2, "like+a+prayer")
+        XCTAssertEqual(converted3, "Moores")
+        XCTAssertEqual(converted4, "Gunsn+Roses")
+        XCTAssertEqual(converted5, "Housecat")
     }
     
 }
